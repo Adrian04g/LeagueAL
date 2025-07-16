59059a753396003c083e23ee94ccd3e9bc3159b1
luizzz-dev <luiscortes0714@gmail.com>
2025-07-15 19:11:58 -0500

    funciones.js

--- main.js
+++ main.js
@@ -1,8 +1,16 @@
+const notif = document.getElementById("notificacion");
+// Agregar un evento al formulario para manejar el envío
 document.getElementById("formTorneo").addEventListener("submit", function(e) {
       e.preventDefault();
       const nombre = document.getElementById("torneo").value;
       const equipos = document.getElementById("opciones").value;
-      alert(`Torneo "${nombre}" creado con ${equipos} equipos.`);
+      //alert(`Torneo "${nombre}" creado con ${equipos} equipos.`);
+      notif.innerText = `✅ Torneo "${nombre}" creado con ${equipos} equipos.`;
+      notif.style.display = "block";
+
+    setTimeout(() => {
+      notif.style.display = "none";
+    }, 4000); 
         // Mostrar la sección después de enviar el formulario
       document.querySelector("section").style.display = "block";
     });
